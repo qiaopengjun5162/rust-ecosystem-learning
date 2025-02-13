aa0ed44163438009b032a1e88f4f1bf306661085
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2025-02-13 14:11:47 +0800

    feature: testcode for anyhow/thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use rust_ecosystem_learning::MyError;
+use std::fs;
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("size of anyhow::Error: {}", size_of::<anyhow::Error>());
+    println!("size of std::io::Error: {}", size_of::<std::io::Error>());
+    println!(
+        "size of std::num::ParseIntError: {}",
+        size_of::<std::num::ParseIntError>()
+    );
+    println!(
+        "size of serde_json::Error: {}",
+        size_of::<serde_json::Error>()
+    );
+    println!("size of string: {}", size_of::<String>());
+    println!("size of MyError: {}", size_of::<MyError>());
+
+    let filename = "non_existent_file.txt";
+    let _fd =
+        fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+
+    fail_with_error()?;
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
 
 #[cfg(test)]
